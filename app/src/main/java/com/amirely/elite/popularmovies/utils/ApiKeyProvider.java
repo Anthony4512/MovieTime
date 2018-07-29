d016adac510dfa0b8f357045bd31349ea637c6f1
Anthony Mirely <anthonymirely@gmail.com>
2018-07-29 18:48:53 -0400

    updated to use custom API key

--- app/src/main/java/com/amirely/elite/popularmovies/utils/ApiKeyProvider.java
+++ app/src/main/java/com/amirely/elite/popularmovies/utils/ApiKeyProvider.java
@@ -1,5 +1,5 @@
 package com.amirely.elite.popularmovies.utils;
 
 public class ApiKeyProvider {
-    public static final String API_KEY = "1b383c179fbd530ae938ea17f25198ae"; //"YOUR API KEY GOES HERE";
+    public static final String API_KEY = "YOUR API KEY GOES HERE";
 }
